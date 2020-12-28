77522f7d5dfabc016751a1ba8f37f72c4223bbf2
Pito0713 <xli30334@gmail.com>
2020-12-28 20:30:57 +0800

    updata

---
+++ src/components/Production.vue
@@ -0,0 +1,24 @@
+<template>
+  <div >
+    <productItem class="slider"/>
+  </div>
+</template>
+
+<script>
+import productItem from '../components/productItem.vue'
+
+export default {
+  components: {
+    productItem
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.slider {
+  display: flex;
+  position: relative;
+  padding-top: 2vw;
+  margin: 5vw 3vw;
+}
+</style>
---
+++ src/product.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <Navbar  id="nav"/>
+    <Production  id="Production"/>
+    <Footer />
+  </div>
+</template>
+<script>
+import Navbar from './components/Navbar.vue'
+import Production from './components/Production.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  data: function () {
+    return {
+      scrollTop: ''
+    }
+  },
+  components: {
+    Navbar,
+    Production,
+    Footer
+  },
+  mounted () {
+    var _this = this
+    window.onscroll = function () {
+      _this.scrollTop =
+        document.documentElement.scrollTop || document.body.scrollTop
+    }
+  },
+  watch: {
+    scrollTop: function () {
+      var nav = document.getElementById('nav')
+      //  var navFix = document.getElementById('navFix')
+      var Production = document.getElementById('Production')
+      var navHeight = nav.offsetHeight
+      if (this.scrollTop > navHeight) {
+        Production.style.marginTop = '68px'
+        nav.style.backgroundColor = 'var(--background-color)'
+        nav.style.position = 'fixed'
+      } else {
+        Production.style.marginTop = '0px'
+        nav.style.position = 'relative'
+        nav.style.backgroundColor = '#ffffff'
+      }
+      var categoryLIst = document.getElementById('categoryLIst')
+      //  var navFix = document.getElementById('navFix')
+      var categoryHeight = categoryLIst.offsetHeight
+      if (this.scrollTop > categoryHeight) {
+        categoryLIst.style.backgroundColor = 'var(--background-color)'
+        categoryLIst.style.position = 'fixed'
+        categoryLIst.style.width = '20%'
+        categoryLIst.style.top = '69px'
+        categoryLIst.style.opacity = '0.7'
+      } else {
+        categoryLIst.style.position = 'relative'
+        categoryLIst.style.backgroundColor = 'var(--product-bg-color)'
+        categoryLIst.style.width = '100%'
+        categoryLIst.style.top = '0px'
+        categoryLIst.style.opacity = '1'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+@import './css/container.css';
+#app {
+  font-family:var(--font-Pragati);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: var(--text-color);
+  background-color: var(--product-bg-color)
+}
+</style>
